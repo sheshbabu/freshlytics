01256c6d9505e1f51f10fad713e65979d6f67769
Sheshbabu <sheshbabu@gmail.com>
2019-06-15 07:18:41 +0800

    Added basic tracker

---
+++ src/client/tracker.js
@@ -0,0 +1,27 @@
+(function() {
+  var COLLECT_URL = "/collect";
+  var PROJECT_ID = 1000;
+  var GLOBAL_VAR_NAME = "__raylight__";
+
+  window[GLOBAL_VAR_NAME] = {};
+
+  window[GLOBAL_VAR_NAME].sendPageView = function() {
+    var path = location.pathname;
+    var referrer = document.referrer;
+
+    var url =
+      COLLECT_URL +
+      "?projectId=" +
+      PROJECT_ID +
+      "&path=" +
+      encodeURIComponent(path) +
+      "&referrer=" +
+      encodeURIComponent(referrer);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.send();
+  };
+
+  window[GLOBAL_VAR_NAME].sendPageView();
+})();
