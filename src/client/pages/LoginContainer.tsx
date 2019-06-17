c007e012e9f402b745f8340ead24aba45e34b29d
Sheshbabu <sheshbabu@gmail.com>
2019-06-17 21:43:17 +0800

    Added api integration

--- src/client/pages/LoginContainer.tsx
+++ src/client/pages/LoginContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Form, Header } from "semantic-ui-react";
+import request from "../request";
 import styles from "./LoginContainer.css";
 
 export default function LoginContainer() {
@@ -12,19 +13,22 @@ export default function LoginContainer() {
 }
 
 function LoginForm() {
+  const [username, setUsername] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
   return (
-    <div>
+    <div className={styles.form}>
       <Header as="h1">Welcome!</Header>
-      <Form className={styles.form}>
+      <Form>
         <Form.Field>
           <label>Username</label>
-          <input />
+          <input name="username" value={username} onChange={e => setUsername(e.target.value)} />
         </Form.Field>
         <Form.Field>
           <label>Password</label>
-          <input type="password" />
+          <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
         </Form.Field>
-        <Button type="submit" color="green">
+        <Button type="submit" color="green" onClick={() => handleSubmit(username, password)}>
           Sign In
         </Button>
       </Form>
@@ -34,4 +38,14 @@ function LoginForm() {
 
 function LoginIllustration() {
   return <div className={styles.illustration} />;
+}
+
+async function handleSubmit(username: string, password: string) {
+  const response = await request("/api/login", "POST", JSON.stringify({ username, password }));
+
+  if (response.shouldForcePasswordChange) {
+    location.replace("/password");
+  } else {
+    location.replace("/");
+  }
 }
