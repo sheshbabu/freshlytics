27acd4c1e4b5194c5de47018c328585fcba21cb4
Sheshbabu <sheshbabu@gmail.com>
2019-06-18 22:27:54 +0800

    Marking all routes except login as private

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import PageViewContainer from "./pages/PageViewContainer";
 import LoginContainer from "./pages/LoginContainer";
 import ChangePasswordContainer from "./pages/ChangePasswordContainer";
@@ -7,9 +7,19 @@ import ChangePasswordContainer from "./pages/ChangePasswordContainer";
 export default function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={PageViewContainer} />
+      <PrivateRoute path="/" exact component={PageViewContainer} />
       <Route path="/login" component={LoginContainer} />
-      <Route path="/password" component={ChangePasswordContainer} />
+      <PrivateRoute path="/password" component={ChangePasswordContainer} />
     </BrowserRouter>
   );
+}
+
+function PrivateRoute(props: any) {
+  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
+
+  if (isAuthenticated) {
+    return <Route {...props} />;
+  } else {
+    return <Redirect to={{ pathname: "/login" }} />;
+  }
 }
