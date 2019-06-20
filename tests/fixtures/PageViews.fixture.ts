eb6d29b6d78266a0995a58c5fddad651525ffa07
Sheshbabu <sheshbabu@gmail.com>
2019-06-20 21:42:01 +0800

    Updated test fixture to add more unique data

--- tests/fixtures/PageViews.fixture.ts
+++ tests/fixtures/PageViews.fixture.ts
@@ -4,10 +4,75 @@ import isBefore from "date-fns/is_before";
 import isEqual from "date-fns/is_equal";
 import PageViews from "../../src/server/db/models/PageViews";
 
-const paths = ["/", "/login", "/signup"];
-const referrers = ["", "https://www.google.com", "https://www.duckduckgo.com"];
-const browserNames = ["Safari", "Firefox", "Chrome"];
-const browserNameVersions = ["Safari 12", "Firefox 67", "Chrome 74"];
+const paths = [
+  "/",
+  "/login",
+  "/signup",
+  "/admin",
+  "/admin/settings",
+  "/admin/users",
+  "/admin/payment",
+  "/admin/integrations",
+  "/admin/notifications",
+  "/repos/web",
+  "/repos/web/settings",
+  "/repos/web/contributors",
+  "/repos/backend",
+  "/repos/backend/settings",
+  "/repos/backend/contributors",
+  "/repos/mobile",
+  "/repos/mobile/settings",
+  "/repos/mobile/contributors",
+  "/repos/docs",
+  "/repos/docs/settings",
+  "/repos/docs/contributors"
+];
+const referrers = [
+  "",
+  "https://www.google.com",
+  "https://www.twitter.com",
+  "https://www.tumblr.com",
+  "https://www.facebook.com",
+  "https://www.youtube.com",
+  "https://www.reddit.com",
+  "https://www.ycombinator.com",
+  "https://www.linkedin.com"
+];
+const browserNames = [
+  "Safari",
+  "Firefox",
+  "Chrome",
+  "Vivaldi",
+  "Opera",
+  "Brave",
+  "Edge",
+  "Internet Explorer",
+  "Netscape",
+  "Pale Moon"
+];
+const browserNameVersions = [
+  "Safari 13",
+  "Safari 12",
+  "Safari 11",
+  "Safari 10",
+  "Safari 09",
+  "Firefox 67",
+  "Firefox 66",
+  "Firefox 65",
+  "Firefox 64",
+  "Firefox 63",
+  "Firefox 62",
+  "Firefox 61",
+  "Firefox 60",
+  "Chrome 74",
+  "Chrome 73",
+  "Chrome 72",
+  "Chrome 71",
+  "Chrome 70",
+  "Chrome 69",
+  "Chrome 68",
+  "Chrome 67"
+];
 
 const startDate = new Date("2018-12-31");
 const endDate = new Date("2020-03-02");
