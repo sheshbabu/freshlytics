7638d051757a1f8bbdefe3e4e3df5e56b10dd817
Sheshbabu <sheshbabu@redmart.com>
2019-08-21 01:54:54 +0800

    Updated Navbar

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <Menu>
-      <Menu.Item header>Freshlytics</Menu.Item>
+    <Menu attached="bottom">
+      <Menu.Item header>
+        <Link to="/" className={styles.header}>
+          freshlytics
+        </Link>
+      </Menu.Item>
+      <Menu.Item position="right">
+        <Link to="/settings" className={styles.link}>
+          Settings
+        </Link>
+      </Menu.Item>
     </Menu>
   );
 }
